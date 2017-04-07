78f9f08af7b903009fc8892e27901610d2ff2787
Josiah Witt <Josiah.Witt@uvm.edu>
2017-04-07 13:56:32 -0400

    Setting up basic main function

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/**
+ * Requires:
+ * Modifies:
+ * Effects:
+*/
+int main(int argc, char const *argv[]) {
+  cout << "Infinity" << endl;
+
+  return 0;
+}
