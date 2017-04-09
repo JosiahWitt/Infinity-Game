f1e7c254e35cd500e7b55019d75658ebe9f370d1
Josiah Witt <Josiah.Witt@uvm.edu>
2017-04-08 22:49:29 -0400

    Added signatures for GameBoard methods that will be implemented later

--- gameboard.hpp
+++ gameboard.hpp
@@ -2,6 +2,13 @@
 #define gameboard_hpp
 
 #include "block.hpp"
+#include "player.hpp"
+#include <map>
+#include <memory>
+#include <random>
+#include <vector>
+
+enum GameDirection { DIR_UP, DIR_DOWN, DIR_LEFT, DIR_RIGHT };
 
 class GameBoard {
 public:
@@ -37,6 +44,41 @@ public:
   int getGamePixelWidth() const;
   int getGamePixelHight() const;
 
+  /**
+  * Requires: a valid path to save the file to
+  * Modifies: nothing
+  * Effects: saves the game to a file
+  */
+  void saveGame(string filename);
+
+  /**
+  * Requires: a valid path to an existing game file
+  * Modifies: all GameBoard fields
+  * Effects: loads the game from a file
+  */
+  void loadGame(string filename);
+
+  /**
+  * Requires: nothing
+  * Modifies: all GameBoard fields
+  * Effects: creates a new game
+  */
+  void newGame();
+
+  /**
+  * Requires: a GameDirection
+  * Modifies: player
+  * Effects: moves the player in the direction specified
+  */
+  void movePlayer(GameDirection direction);
+
+  /**
+  * Requires: an integer position x and an integer position y
+  * Modifies: board
+  * Effects: maps the given positions to the grid and moves the wall
+  */
+  void moveWall(int fromX, int fromY, int toX, int toY);
+
 private:
   // Number of blocks to render for the height and for the width -- set at
   // object construction
@@ -45,6 +87,26 @@ private:
   // The width and height of each block -- set at object construction
   int blockWidth;
   int blockHeight;
+
+  // Store the seed used to generate the board
+  int seed;
+
+  // Store the 2D vector of pointers to the blocks on the board
+  vector<vector<unique_ptr<Block>>> board;
+
+  // Store a map of changes to the default blocks
+  //  Note: 'n:m' maps to the element at board[n][m]
+  map<string, unique_ptr<Block>> changes;
+
+  // Store the player object
+  Player player;
+
+  /**
+  * Requires: nothing
+  * Modifies: board
+  * Effects: generates the board based on the seed
+  */
+  void generateBoard();
 };
 
 #endif
